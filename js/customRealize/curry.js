fd304654ae579468c6f9c3dfb749d8d1bdf94d07
yao.nie <yao.nie@mail.hypers.com>
2021-03-01 11:18:25 +0800

    添加一种curry函数实现

--- js/customRealize/curry.js
+++ js/customRealize/curry.js
@@ -4,13 +4,23 @@
 //     ...args,
 //   ]);
 
-function curry(fn) {
-  return function currify() {
-    const args = Array.prototype.slice.call(arguments);
-    if (args.length >= fn.length) {
-      return fn.apply(null, args);
-    }
-    return currify.bind(null, ...args);
+// function curry(fn) {
+//   return function currify() {
+//     const args = Array.prototype.slice.call(arguments);
+//     if (args.length >= fn.length) {
+//       return fn.apply(null, args);
+//     }
+//     return currify.bind(null, ...args);
+//   };
+// }
+
+function curry(fn, ...args) {
+  if (args.length >= fn.length) {
+    return fn(...args);
+  }
+
+  return function (...args2) {
+    return curry(fn, ...args, ...args2);
   };
 }
 
