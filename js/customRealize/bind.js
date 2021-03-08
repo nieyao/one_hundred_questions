de62da3a6367dfa86be35f44dcaeae3b597e922a
nieyao <1161781399@qq.com>
2021-03-08 22:56:47 +0800

    完善 bind 函数实现

--- js/customRealize/bind.js
+++ js/customRealize/bind.js
@@ -12,6 +12,26 @@ Function.prototype.myBind = function (context, ...args1) {
   };
 };
 
+// 第四版
+Function.prototype.myBind = function (context) {
+  var self = this;
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  var fNOP = function () {};
+
+  var fBound = function () {
+    var bindArgs = Array.prototype.slice.call(arguments);
+    return self.apply(
+      this instanceof fNOP ? this : context,
+      args.concat(bindArgs)
+    );
+  };
+
+  fNOP.prototype = this.prototype;
+  fBound.prototype = new fNOP();
+  return fBound;
+};
+
 let value = 2;
 let foo = {
   value: 1,
