06833d74697aec0aff0411b0fc993a98f97462fd
nieyao <1161781399@qq.com>
2021-03-08 23:14:37 +0800

    完善 new 函数实现

--- js/customRealize/new.js
+++ js/customRealize/new.js
@@ -1,5 +1,23 @@
 function _new(constructor, ...arguments) {
   var obj = Object.create(constructor.prototype);
   const ret = constructor.apply(obj, arguments);
-  return typeof ret === "object" ? ret : obj;
+  return typeof ret === "object" ? ret || obj : obj;
 }
+
+// 测试
+function Otaku(name, age) {
+  this.strength = 60;
+  this.age = age;
+
+  return {
+    name: name,
+    habit: "Games",
+  };
+}
+
+var person = _new(Otaku, "Kevin", "18");
+
+console.log(person.name); // Kevin
+console.log(person.habit); // Games
+console.log(person.strength); // undefined
+console.log(person.age); // undefined
