829b12d2ff9e17ed338853903373da4ed497b6cc
nieyao <1161781399@qq.com>
2021-03-17 23:02:21 +0800

    实现 promise.all

---
+++ code/promise.all.js
@@ -0,0 +1,60 @@
+/**
+ * 实现一个 Promise.all
+ */
+
+function promiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(promises)) {
+      return reject(new TypeError("arguments must be an array"));
+    }
+    let resolvedCounter = 0;
+    let promiseNum = promises.length;
+    let resolvedValues = new Array(promiseNum);
+    for (let i = 0; i < promiseNum; i++) {
+      Promise.resolve(promises[i]).then(
+        (value) => {
+          resolvedCounter++;
+          resolvedValues[i] = value;
+          if (resolvedCounter == promiseNum) {
+            return resolve(resolvedValues);
+          }
+        },
+        (reason) => {
+          return reject(reason);
+        }
+      );
+    }
+  });
+}
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 300, "foo");
+});
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "bar");
+});
+
+const promise5 = Promise.reject("err");
+const promises = [promise1, promise2, promise3, promise4];
+// expected output: Array [3, 42, "foo", "bar"]
+
+const promises1 = [promise1, promise2, promise3, promise4, promise5];
+// expected output: "err"
+
+promiseAll(promises)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+promiseAll(promise1)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
