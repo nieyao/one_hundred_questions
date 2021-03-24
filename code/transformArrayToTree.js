e6d07ddae3c13875c7c95945102dbe4cba21b00c
yao.nie <yao.nie@mail.hypers.com>
2021-03-24 18:09:52 +0800

    update questions

---
+++ code/transformArrayToTree.js
@@ -0,0 +1,71 @@
+/**
+ * 将一个数组转成树状的对象
+ * @param {Array} arr
+ */
+function transformArrayToTree(arr) {
+  if (!arr || !(arr instanceof Array)) return "错误的数据类型";
+  if (!arr.length) return "空数组";
+  var len = arr.length;
+  var rootObj = { id: null, name: null, children: [] };
+  var nodeObj = {};
+  for (var i = 0; i < len; i++) {
+    if (!arr[i].parentId) {
+      rootObj = {
+        id: arr[i].id,
+        name: arr[i].name,
+        children: [],
+      };
+    } else {
+      if (nodeObj.hasOwnProperty(arr[i].parentId)) {
+        nodeObj[arr[i].parentId].children.push(arr[i]);
+      } else {
+        nodeObj[arr[i].parentId] = {};
+        nodeObj[arr[i].parentId].children = [];
+        nodeObj[arr[i].parentId].children.push(arr[i]);
+      }
+    }
+  }
+  // 整理根节点过程
+  function getChildren(node) {
+    if (nodeObj[node.id] && nodeObj[node.id].children) {
+      node.children = nodeObj[node.id].children;
+      delete nodeObj[node.id];
+      var len = node.children.length;
+      if (len > 0) {
+        for (var i = 0; i < len; i++) {
+          getChildren(node.children[i]);
+        }
+      }
+    } else if (!nodeObj[node.id]) {
+      console.log(node.id + "没有children");
+    }
+  }
+  getChildren(rootObj);
+  for (var p in nodeObj) {
+    if (nodeObj.hasOwnProperty) {
+      console.warn(p + ":没有该父节点");
+    }
+  }
+  return rootObj;
+}
+
+let arr = [
+  { id: 1, name: "i1" },
+  { id: 2, name: "i2", parentId: 1 },
+  { id: 4, name: "i4", parentId: 3 },
+  { id: 3, name: "i3", parentId: 2 },
+];
+
+const obj = transformArrayToTree(arr);
+// 输入 1
+console.log(obj);
+
+let a = {
+  id: 1,
+  name: "i1",
+  children: {
+    id: 2,
+    name: "i2",
+    children: {},
+  },
+};
