1b6bbafcd08227d85a13cd753391e87d543a61b6
nieyao <1161781399@qq.com>
2021-03-24 23:26:06 +0800

    新增 getShortDistance 函数

---
+++ code/getShortDistance.js
@@ -0,0 +1,38 @@
+/**
+ * TreeNode:{
+ *    this.left;
+ *    this.right;
+ *    this.parent;
+ *    this.depth;
+ * }
+ * @param {TreeNode} a
+ * @param {TreeNode} b
+ * @returns
+ */
+var getShortDistance = function (a, b) {
+  let aDis = 0;
+  let bDis = 0;
+  const handle = (a, b) => {
+    if (!a || !b || a === b) {
+      return;
+    }
+
+    if (a.depth > b.depth) {
+      aDis++;
+      handle(a.parent, b);
+    }
+
+    if (a.depth < b.depth) {
+      bDis++;
+      handle(a, b.parent);
+    }
+
+    if (a.depth === b.depth) {
+      aDis++;
+      bDis++;
+      handle(a.parent, b.parent);
+    }
+  };
+  handle(a, b);
+  return aDis + bDis;
+};
